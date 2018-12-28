bd8cc26d61f095a74864c76391b239f6fa972cbe
Karyna <karina.labazova@gmail.com>
2018-12-28 19:46:27 +0200

    Update lesson-08.js

--- lesson-08.js
+++ lesson-08.js
@@ -8,15 +8,15 @@ request.open (
 request.onload = function(){
 	shapes = JSON.parse(this.responseText)
 	.forEach(
-	   function (item){
-		     var image = document.createElement('img')
-		     document.body.appendChild(image)
-		     image.src = item.ref
-		     image.style.width = '100px'
-		     var title = document.createElement('p')
-		     document.body.appendChild(title)
-		     title.innerHTML = item.title
-	   }
- )
+		function (item){
+			var image = document.createElement('img')
+			document.body.appendChild(image)
+			image.src = item.ref
+			image.style.width = '100px'
+			var title = document.createElement('p')
+			document.body.appendChild(title)
+			title.innerHTML = item.title
+		}
+	)
 }
 request.send()
